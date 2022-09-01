f02cff7dd3f8ebb95b633bd5fed7df4870a59e80
Ivan Garcia <ivanggv2@gmail.com>
2022-09-01 16:02:23 +0200

    Added the ThemeProvider to index.tsx

--- src/styles/styledMainTheme.tsx
+++ src/styles/styledMainTheme.tsx
@@ -1,9 +1,9 @@
 const styledMainTheme = {
-  firstColor: "F4EBEB",
-  secondColor: "F5D5D5",
-  thirdColor: "E99E9E",
-  fontMainColor: "F330000",
-  fontSecondColor: "A00043",
+  firstColor: "#F4EBEB",
+  secondColor: "#F5D5D5",
+  thirdColor: "#E99E9E",
+  fontMainColor: "#330000",
+  fontSecondColor: "#A00043",
   fontMainType: "Chewy",
   fontSeconType: "Roboto",
 };
