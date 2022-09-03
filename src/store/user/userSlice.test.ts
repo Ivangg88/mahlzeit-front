ee7ddd277263a6094969bdd686f2ed63e6a260ff
Ivan Garcia <ivanggv2@gmail.com>
2022-09-03 10:54:41 +0200

    Added test for reducer to the test for userSlices

--- src/store/user/userSlice.test.ts
+++ src/store/user/userSlice.test.ts
@@ -1,4 +1,5 @@
-import { loginUserActionCreator } from "./userSlice";
+import { UserLogged } from "../../types/interfaces";
+import { loginUserActionCreator, userReducer } from "./userSlice";
 
 describe("Given a loginUserActionCreator", () => {
   describe("When is called", () => {
@@ -12,6 +13,27 @@ describe("Given a loginUserActionCreator", () => {
       const action = loginUserActionCreator(true);
 
       expect(action).toStrictEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a userReducer function", () => {
+  describe("When is called with an action type 'user/loginUser' and a payload 'true", () => {
+    test("Then it should return a copy of the previousState with the property isLogged = true", () => {
+      const previousUser: UserLogged = {
+        userName: "",
+        isLogged: false,
+      };
+      const expectedUser = {
+        ...previousUser,
+        isLogged: true,
+      };
+
+      const action = loginUserActionCreator(true);
+
+      const receivdUser = userReducer(previousUser, action);
+
+      expect(receivdUser).toStrictEqual(expectedUser);
     });
   });
 });
