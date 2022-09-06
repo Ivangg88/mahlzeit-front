6fc133696fe237bff39b1631b74ab49a758a6cf7
Ivan Garcia <ivanggv2@gmail.com>
2022-09-06 13:32:10 +0200

    Added header to the pages

--- src/pages/LoginPage/LoginPageStyled.tsx
+++ src/pages/LoginPage/LoginPageStyled.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const LoginPageStyled = styled.div`
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
 `;
 
 export default LoginPageStyled;
--- src/pages/ResgisterPage/RegisterPageStyled.tsx
+++ src/pages/ResgisterPage/RegisterPageStyled.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const RegisterPageStyled = styled.div`
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
 `;
 
 export default RegisterPageStyled;
