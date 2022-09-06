70fcd844c20f66dd9efb8c1a9eae079ffd44139a
Ivan Garcia <ivanggv2@gmail.com>
2022-09-06 13:26:05 +0200

    Created component HEader

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+import HeaderStyled from "./HeaderStyled";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <div className="header__text-container">
+        <h1 className="header__title">Mahlzeit</h1>
+        <ul className="header__navigation">
+          <li>
+            <NavLink className="navigation__link" to={"/home"}>
+              Inicio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navigation__link" to={"/myReciptes"}>
+              Mis Recetas
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navigation__link" to={"/createRecipte"}>
+              Crear Receta
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navigation__link" to={"/login"}>
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <img
+        src="/images/logo.png"
+        alt="Logo mahlzeit"
+        height={96}
+        className="header__logo"
+      />
+    </HeaderStyled>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/HeaderStyled.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  width: 400px;
+  height: 100px;
+  border-radius: 15px;
+  border: 1px solid ${(props) => props.theme.fontMainColor};
+  color: ${(props) => props.theme.fontMainColor};
+  background-color: ${(props) => props.theme.firstColor};
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+
+  .header__logo {
+    background-image: url(/images/logo.png);
+    border-radius: 15px;
+  }
+
+  .header__text-container {
+    flex: 4;
+  }
+
+  a:visited,
+  a:link,
+  a:active {
+    text-decoration: none;
+    color: ${(props) => props.theme.fontMainColor};
+    font-weight: bold;
+  }
+
+  .header__title {
+    margin: 0;
+    text-align: center;
+  }
+
+  li {
+    list-style: none;
+    display: inline;
+  }
+
+  .header__navigation {
+    display: flex;
+    gap: 25px;
+  }
+
+  .navigation__link {
+    display: flex;
+    text-align: center;
+  }
+`;
+
+export default HeaderStyled;
