9c5e4f4c0cc9aeaa627b62a4b58cf49437f6941f
Ivan Garcia <ivanggv2@gmail.com>
2022-09-07 19:54:01 +0200

    Created HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import ItemList from "./components/ItemList/ItemList";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import RegisterPage from "./pages/ResgisterPage/RegisterPage";
 const App = (): JSX.Element => {
   return (
-    <>
+    /*<>
       <Routes>
         <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/home" element={<Navigate to="/register" />} />
@@ -12,7 +13,8 @@ const App = (): JSX.Element => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </>
+      </>*/
+    <ItemList />
   );
 };
 
