fe5283f5134dbdd5a21fbf90fff6e5e8853aef20
Ivan Garcia <ivanggv2@gmail.com>
2022-09-10 18:31:27 +0200

    Modifiied the type of the received data from the API

--- src/hooks/useReciptes.ts
+++ src/hooks/useReciptes.ts
@@ -1,7 +1,6 @@
 import { useCallback } from "react";
 import { useAppDispatch } from "../app/hooks";
 import { loadReciptesActionCreator } from "../store/recipte/recipteSlice";
-import { Recipte } from "../types/interfaces";
 
 const useReciptes = () => {
   const dispatch = useAppDispatch();
@@ -10,9 +9,9 @@ const useReciptes = () => {
     async (apiUrl: string) => {
       try {
         const response = await fetch(apiUrl);
-        const items: Recipte[] = await response.json();
+        const { reciptes } = await response.json();
 
-        dispatch(loadReciptesActionCreator(items));
+        dispatch(loadReciptesActionCreator(reciptes));
       } catch (error) {
         return error;
       }
