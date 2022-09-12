c1865e9b66770a92bc2928439db4a98e3e2cefe1
Ivan Garcia <ivanggv2@gmail.com>
2022-09-12 12:47:04 +0200

    Added test for the loadRecipteActionCreator

--- src/store/recipte/recipteSlice.test.ts
+++ src/store/recipte/recipteSlice.test.ts
@@ -1,7 +1,11 @@
 import { Recipte } from "../../types/interfaces";
-import { reciptesReducer, loadReciptesActionCreator } from "./recipteSlice";
+import {
+  reciptesReducer,
+  loadReciptesActionCreator,
+  loadRecipteActionCreator,
+} from "./recipteSlice";
 
-describe("Given a loadREciptesActionCreator", () => {
+describe("Given a loadReciptesActionCreator", () => {
   describe("When is called", () => {
     test("Then it should return an action with the type recipte/loadReciptes", () => {
       const actionType = "recipte/loadReciptes";
@@ -29,7 +33,35 @@ describe("Given a loadREciptesActionCreator", () => {
   });
 });
 
-describe("Given a itemReducer function", () => {
+describe("Given a loadRecipteActionCreator", () => {
+  describe("When is called", () => {
+    test("Then it should return an action with the type recipte/loadRecipte", () => {
+      const actionType = "recipte/loadRecipte";
+      const recipte: Recipte = {
+        id: "",
+        name: "",
+        dificulty: "Medio",
+        autor: "",
+        image: "",
+        persons: 0,
+        ingredients: "",
+        process: "",
+        backupImage: "",
+      };
+
+      const expectedAction = {
+        type: actionType,
+        payload: recipte,
+      };
+
+      const action = loadRecipteActionCreator(recipte);
+
+      expect(action).toStrictEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a reciptesReducer function", () => {
   describe("When is called with an action type 'item/loadItems' and a payload with an item", () => {
     test("Then it should return a copy of the loaded items", () => {
       const items: Recipte[] = [
