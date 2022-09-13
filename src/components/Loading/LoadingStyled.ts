596a9e28e99e0129f2781d646918700819dbd38d
Ivan Garcia <ivanggv2@gmail.com>
2022-09-13 21:19:49 +0200

    Modified style of Loading

--- src/components/Loading/LoadingStyled.ts
+++ src/components/Loading/LoadingStyled.ts
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 
 const LoadingStyled = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  min-width: fit-content;
-  min-height: 100%;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
