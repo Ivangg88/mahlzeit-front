c7e716d1f930c8f8ae7fd6af24ce855b789cc005
Ivan Garcia <ivanggv2@gmail.com>
2022-09-14 16:40:18 +0200

    Created test for App

---
+++ src/App.test.tsx
@@ -0,0 +1,27 @@
+import { screen } from "@testing-library/react";
+import App from "./App";
+import renderWithProviders from "./utils/testStore";
+
+describe("Given a component App", () => {
+  describe("When rendered without a logged user", () => {
+    test("It should show the component Header with the link 'Login'", () => {
+      renderWithProviders(<App />);
+
+      const link = screen.getByRole("link", { name: "Login" });
+
+      expect(link).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered with a logged user", () => {
+    test("It should show the component Header with a link 'Logout'", () => {
+      renderWithProviders(<App />, {
+        preloadedState: { user: { token: "", userName: "", isLogged: true } },
+      });
+
+      const link = screen.getByRole("link", { name: "Logout" });
+
+      expect(link).toBeInTheDocument();
+    });
+  });
+});
