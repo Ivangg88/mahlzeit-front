f6eb5414078705767c5c42de437708f8ca93bb5a
Ivan Garcia <ivanggv2@gmail.com>
2022-09-14 11:55:29 +0200

    Added case to test

--- src/pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -3,15 +3,33 @@ import { BrowserRouter } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 
 describe("Given a component NotFoundPage", () => {
-  describe("When rendered", () => {
+  describe("When rendered with a error false", () => {
     test("Then it should show a container with the texts 'Error 404' and 'Page not found'", () => {
       const text1 = "Error 404";
-      const text2 = "Page not found";
+      const text2 = "Página no encontrada";
 
       render(
         <BrowserRouter>
-          {" "}
-          <NotFoundPage />
+          <NotFoundPage error={false} />
+        </BrowserRouter>
+      );
+
+      const expectedText1 = screen.getByText(text1);
+      const expectedText2 = screen.getByText(text2);
+
+      expect(expectedText1).toBeInTheDocument();
+      expect(expectedText2).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendered with a error true", () => {
+    test("Then it should show a container with the texts 'Error 404' and 'Page not found'", () => {
+      const text1 = "Error general";
+      const text2 = "No se encuentra el recurso";
+
+      render(
+        <BrowserRouter>
+          <NotFoundPage error={true} />
         </BrowserRouter>
       );
 
