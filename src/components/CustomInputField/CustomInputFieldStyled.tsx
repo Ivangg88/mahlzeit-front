b81a510d83837146e3be47252e62c90bb5370271
Ivan Garcia <ivanggv2@gmail.com>
2022-11-30 19:15:34 +0100

    created component CustomInputFIeld

---
+++ src/components/CustomInputField/CustomInputFieldStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const CustomInputFieldStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+export default CustomInputFieldStyled;
