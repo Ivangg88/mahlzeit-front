a7e733bd8a2ca04eb9e9c40ac8781313e9833b4c
Ivan Garcia <ivanggv2@gmail.com>
2022-11-30 20:33:01 +0100

    Created component Login with Formik

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,32 @@
+import { Formik } from "formik";
+import useUsers from "../../hooks/useUsers";
+import { validateLoginSchema } from "../../schemas/validateLoginSchema";
+import { UserLogin } from "../../types/interfaces";
+import LoginFormikForm from "../LoginFormikForm/LoginFormikForm";
+
+const initialValues: UserLogin = { userName: "", password: "" };
+const apiUrl = process.env.REACT_APP_API_URL!;
+interface LoginProps {
+  navigateTarget: string;
+}
+
+const Login = ({ navigateTarget }: LoginProps): JSX.Element => {
+  const { loginUser } = useUsers();
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validateLoginSchema}
+      onSubmit={(values, { resetForm }) => {
+        (async () => {
+          if (await loginUser(values, apiUrl, navigateTarget)) {
+            resetForm();
+          }
+        })();
+      }}
+    >
+      <LoginFormikForm />
+    </Formik>
+  );
+};
+
+export default Login;
