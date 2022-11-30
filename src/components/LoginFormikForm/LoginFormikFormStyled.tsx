2477d80652d7a7d27208e506dd39ee20b7f45a00
Ivan Garcia <ivanggv2@gmail.com>
2022-11-30 20:30:46 +0100

    Created component LoginFormikForm

---
+++ src/components/LoginFormikForm/LoginFormikFormStyled.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+const LoginFormikFormStyled = styled.div`
+  width: 400px;
+  height: 620px;
+  display: flex;
+  justify-content: center;
+  background-color: ${(props) => props.theme.thirdColor};
+  font-family: ${(props) => props.theme.fontSecondType};
+  color: ${(props) => props.theme.fontMainColor};
+  border-radius: 15px;
+
+  .login-form {
+    font-family: inherit;
+    color: inherit;
+    padding: 20px;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+
+    &__title {
+      font-family: ${(props) => props.theme.fontMainType};
+      font-size: 30px;
+      margin: 0;
+    }
+
+    &__inputs-container {
+      margin-top: 20px;
+      margin-bottom: 30px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 100%;
+      gap: 20px;
+    }
+
+    &__input {
+      width: 100%;
+      font-family: inherit;
+      height: 37px;
+      padding-left: 5px;
+      border: none;
+      border-radius: 5px;
+    }
+
+    &__navigator {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 10px;
+    }
+  }
+`;
+
+export default LoginFormikFormStyled;
