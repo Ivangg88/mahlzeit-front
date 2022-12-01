cab95cc8ede22e5e18bafb4cbaab30e680cce7ec
Ivan Garcia <ivanggv2@gmail.com>
2022-12-01 17:35:19 +0100

    Added style for desktop version

--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -41,12 +41,48 @@ const HeaderStyled = styled.header`
 
   .header__navigation {
     display: flex;
+    justify-content: space-around;
     gap: 25px;
+    padding-left: 10px;
   }
 
   .navigation__link {
     display: flex;
+    justify-content: space-around;
     text-align: center;
+  }
+
+  a.active {
+    color: #a00043;
+    font-size: 120%;
+  }
+
+  @media (min-width: 450px) {
+    width: 100%;
+    border: none;
+    border-radius: 0;
+    padding-left: 20px;
+    font-size: 1.2rem;
+
+    .header__text-container {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      flex: 1;
+    }
+
+    .header__navigation {
+      display: flex;
+      justify-content: flex-start;
+      gap: 20px;
+      padding: 0px 40px;
+    }
+
+    .header__logo {
+      background-image: url(/images/logo.png);
+      border-radius: 15px;
+      float: left;
+    }
   }
 `;
 
