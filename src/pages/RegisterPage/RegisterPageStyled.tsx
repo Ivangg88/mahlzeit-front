afd1e61ace90f33e1188f2d2a2874a5cfc109bd9
Ivan Garcia <ivanggv2@gmail.com>
2022-12-03 14:33:18 +0100

    Modified the name

---
+++ src/pages/RegisterPage/RegisterPageStyled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const RegisterPageStyled = styled.div`
+  width: 450px;
+  padding: 20px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+
+  @media (min-width: 450px) {
+    width: 100%;
+    padding: 10px 0;
+  }
+`;
+
+export default RegisterPageStyled;
